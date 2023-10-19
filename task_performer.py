a2853b71f9018dc82cbdc67509fd44d95a4db10e
Erik M Jacobs <erikmjacobs@gmail.com>
2023-10-19 15:31:52 -0400

    hard-coded task performer

--- task_performer.py
+++ task_performer.py
@@ -3,11 +3,51 @@ import sys
 from string import Template
 from model_context import get_watsonx_predictor
 
+DEFAULT_MODEL = "ibm/granite-13b-chat-grounded-v01"
+
 
 class TaskPerformer:
-    def perform_task(self, conversation, model, task, original_query):
+    def __init__(self):
+        logging.basicConfig(
+            stream=sys.stdout,
+            format="%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+            level=logging.INFO,
+        )
+        self.logger = logging.getLogger("task_rephraser")
 
-        return """
+    def perform_task(self, conversation, task, **kwargs):
+        if "model" in kwargs:
+            model = kwargs["model"]
+        else:
+            model = DEFAULT_MODEL
+
+        if "verbose" in kwargs:
+            if kwargs["verbose"] == "True" or kwargs["verbose"] == "true":
+                verbose = True
+            else:
+                verbose = False
+        else:
+            verbose = False
+
+        # TODO: must be a smarter way to do this
+        settings_string = Template(
+            '{"conversation": "$conversation", "task": "$task", "model": "$model", "verbose": "$verbose"}'
+        )
+
+        self.logger.info(
+            conversation
+            + " call settings: "
+            + settings_string.substitute(
+                conversation=conversation, task=task, model=model, verbose=verbose
+            )
+        )
+
+        # determine if this should go to a general LLM, the YAML generator, or elsewhere
+
+        # send to the right tool
+
+        # output the response
+        response = """
 apiVersion: "autoscaling.openshift.io/v1"
 kind: "ClusterAutoscaler"
 metadata:
@@ -18,3 +58,45 @@ spec:
   scaleDown: 
     enabled: true 
 """
+
+        self.logger.info(conversation + " response: " + response)
+
+        # return the response
+
+        return response
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Perform a task")
+    parser.add_argument(
+        "-c",
+        "--conversation-id",
+        default="1234",
+        type=str,
+        help="A short identifier for the conversation",
+    )
+    parser.add_argument(
+        "-t",
+        "--task",
+        default="Create an autoscaler YAML that scales the cluster up to 10 nodes",
+        type=str,
+        help="A task to perform",
+    )
+    parser.add_argument(
+        "-m", "--model", default=DEFAULT_MODEL, type=str, help="The model to use"
+    )
+    parser.add_argument(
+        "-v",
+        "--verbose",
+        default=False,
+        help="Set Verbose status of langchains [True/False]",
+    )
+
+    args = parser.parse_args()
+
+    task_performer = TaskPerformer()
+    task_performer.perform_task(
+        args.conversation_id, args.task, model=args.model, verbose=args.verbose
+    )
