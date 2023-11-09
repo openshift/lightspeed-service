d5de07f0451dbb23ee42a756805ab06769ad0133
Erik M Jacobs <erikmjacobs@gmail.com>
2023-11-09 10:37:06 -0500

    removes the old task breakdown endpoint and promotes the new one

--- ols.py
+++ ols.py
@@ -22,7 +22,7 @@ import uuid
 from modules.model_context import get_watsonx_predictor
 
 instruct_model = os.getenv("INSTRUCT_MODEL", "ibm/granite-13b-instruct-v1")
-rag_model = os.getenv("RAG_MODEL", "ibm/granite-13b-chat-grounded-v01")
+rag_model = os.getenv("RAG_MODEL", "ibm/granite-13b-chat-v1")
 
 # TODO: env for verbose chains
 
@@ -46,60 +46,6 @@ def read_root():
 
 
 @app.post("/ols")
-def ols_request(llm_request: LLMRequest):
-    # TODO: probably should setup all models to use here to avoid setting them
-    # up over and over
-
-    # generate a unique UUID for the request:
-    conversation = get_suid()
-    llm_response = LLMRequest(query=llm_request.query)
-    llm_response.conversation_id = conversation
-
-    logging.info(conversation + " New conversation")
-
-    # TODO: some kind of logging module that includes the conversation automatically?
-    logging.info(conversation + " Incoming request: " + llm_request.query)
-
-    # determine what tasks are required to perform the query
-    task_breakdown = TaskBreakdown()
-
-    # TODO: make a real task breakdown call
-    # task_list, referenced_documents = task_breakdown.breakdown_tasks(
-    #   conversation, llm_request.query
-    # )
-
-    # TODO: remove fake response
-    task_list = [
-        "1. Create a ClusterAutoscaler YAML that specifies the maximum size of the cluster"
-    ]
-
-    # task_list = [
-    #    "1. Create a ClusterAutoscaler YAML that specifies the maximum size of the cluster",
-    #    "2. Create a MachineAutoscaler YAML object to specify which MachineSet should be scaled and the minimum and maximum number of replicas."
-    # ]
-
-    logging.info(conversation + " Task list: " + str(task_list))
-
-    # determine if the various tasks can be performed with the information given
-    # TODO: should this maybe be called task validator?
-    task_processor = TaskProcessor()
-    processed = task_processor.process_tasks(conversation, task_list, llm_request.query)
-
-    # when the 0th element is 0, the task processor has encountered an error, likely that it can't figure out what to do
-    # with the provided information in the query. alert the user
-    if processed[0] == 0:
-        llm_response.response = processed[1]
-        raise HTTPException(status_code=500, detail=llm_response.dict())
-
-    # TODO: handle a response of a 9 which means there was some other issue in understanding the task
-
-    llm_response.response = processed[1]
-
-    # if we got this far, all of the tasks can be completed, so do something
-    return llm_response
-
-
-@app.post("/ols2")
 def ols2_request(llm_request: LLMRequest):
     # this endpoint is for the alternative flow
     # 1. validate whether the query is about k8s/ocp
