dd79507f8ac4f3254a346421a144a30cce7b0ff7
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-12-15 09:15:56 +0100

    Unit tests for parse_and_check_json_markdown function

--- tests/unit/utils/test_json_tools.py
+++ tests/unit/utils/test_json_tools.py
@@ -1,5 +1,6 @@
 import pytest
 from utils.json_tools import parse_json_markdown
+from utils.json_tools import parse_and_check_json_markdown
 
 
 correct_inputs = (
@@ -37,3 +38,53 @@ def test_parse_json_markdown_negative_test_cases(json):
     """Check parsing incorrect input."""
     with pytest.raises(Exception):
         parse_json_markdown(json)
+
+
+@pytest.mark.parametrize("json,expected", correct_inputs)
+def test_parse_and_check_json_markdown(json, expected):
+    """Check parsing incorrect input."""
+    json_obj = parse_and_check_json_markdown(json, [])
+    assert json_obj is not None
+    assert parse_json_markdown(json) == expected
+
+
+correct_inputs_with_keys = (
+    ("{}", {}, []),
+    ('{"x":42}', {"x": 42}, ["x"]),
+    ('{"x":42, "y":""}', {"x": 42, "y": ""}, ["x", "y"]),
+    ('{"x":42, "y":"", "z":[]}', {"x": 42, "y": "", "z": []}, ["x", "y", "z"]),
+    ('   {"x":42, "y":""}   ', {"x": 42, "y": ""}, ["x", "y"]),
+    ('```{"x":42, "y":""}```', {"x": 42, "y": ""}, ["x", "y"]),
+    ('\n{"x":42, "y":""}\n', {"x": 42, "y": ""}, ["x", "y"]),
+    ('\t{"x":42, "y":""}\t', {"x": 42, "y": ""}, ["x", "y"]),
+    ('```json{"x":42, "y":""}```', {"x": 42, "y": ""}, ["x", "y"]),
+)
+
+
+@pytest.mark.parametrize("json,expected,keys", correct_inputs_with_keys)
+def test_parse_and_check_json_markdown_check_keys(json, expected, keys):
+    """Check parsing correct input and checking if resulting object contains given keys."""
+    json_obj = parse_and_check_json_markdown(json, keys)
+    assert json_obj is not None
+    assert parse_json_markdown(json) == expected
+
+
+correct_inputs_with_unexpected_keys = (
+    ('{"x":42}', {"x": 42}, ["foo"]),
+    ('{"x":42}', {"x": 42}, ["foo", "bar", "baz"]),
+    ('{"x":42}', {"x": 42}, ["x", "foo"]),
+)
+
+
+@pytest.mark.parametrize("json,expected,keys", correct_inputs_with_unexpected_keys)
+def test_parse_and_check_json_markdown_check_for_unexpected_keys(json, expected, keys):
+    """Check parsing incorrect input."""
+    with pytest.raises(ValueError):
+        parse_and_check_json_markdown(json, keys)
+
+
+@pytest.mark.parametrize("json", incorrect_inputs)
+def test_parse_and_check_json_markdown_negative_test_cases(json):
+    """Check parsing incorrect input."""
+    with pytest.raises(ValueError):
+        parse_and_check_json_markdown(json, [])
