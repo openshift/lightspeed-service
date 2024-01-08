41542388aee43323e1366326b7062b0b3a759c7a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-08 11:54:34 +0100

    Two new integration tests for query post endpoint

--- tests/integration/test_ols.py
+++ tests/integration/test_ols.py
@@ -1,8 +1,10 @@
 import requests
 from fastapi.testclient import TestClient
 
+from app import constants
 from app.endpoints import ols
 from app.main import app
+from src.query_helpers.question_validator import QuestionValidator
 
 client = TestClient(app)
 
@@ -58,4 +60,48 @@ def test_raw_prompt(monkeypatch) -> None:
         "conversation_id": "1234",
         "query": "test query",
         "response": "test response",
+    }
+
+
+def test_post_question_on_unexpected_payload() -> None:
+    """Check the REST API /ols/ with POST HTTP method when unexpected payload is posted."""
+
+    response = client.post("/ols/", json="this is really not proper payload")
+    print(response)
+    assert response.status_code == requests.codes.unprocessable
+    assert response.json() == {
+        "detail": [
+            {
+                "input": "this is really not proper payload",
+                "loc": ["body"],
+                "msg": "Input should be a valid dictionary or object to extract fields from",
+                "type": "model_attributes_type",
+                "url": "https://errors.pydantic.dev/2.5/v/model_attributes_type",
+            }
+        ],
+    }
+
+
+def test_post_question_on_invalid_question(monkeypatch) -> None:
+    """Check the REST API /ols/ with POST HTTP method for invalid question."""
+
+    def dummy_validator(
+        self, conversation: str, query: str, verbose: bool = False
+    ) -> list[str]:
+        return constants.INVALID, "anything"
+
+    # let's pretend the question is invalid without even asking LLM
+    monkeypatch.setattr(QuestionValidator, "validate_question", dummy_validator)
+
+    response = client.post(
+        "/ols/", json={"conversation_id": "1234", "query": "test query"}
+    )
+    print(response)
+    assert response.status_code == requests.codes.unprocessable
+    assert response.json() == {
+        "detail": {
+            "response": "Sorry, I can only answer questions about OpenShift "
+            "and Kubernetes. This does not look like something I "
+            "know how to handle."
+        }
     }
