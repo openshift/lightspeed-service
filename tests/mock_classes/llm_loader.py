dd4cf7d317972b30bfb4d6411a9a7bc9256f3523
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-09 10:47:49 +0100

    Docstrings in all mocks

--- tests/mock_classes/llm_loader.py
+++ tests/mock_classes/llm_loader.py
@@ -1,9 +1,17 @@
+"""Mock for LLMLoader to be used in unit tests."""
+
+
 class MockLLMLoader:
+    """Mock for LLMLoader."""
+
     def __init__(self, llm=None):
+        """Constructor that just stores the selected LLM into object's attribute."""
         self.llm = llm
 
 
 def mock_llm_loader(llm=None):
+    """Constructs mock for LLMLoader."""
+
     def loader(*args, **kwargs):
         return MockLLMLoader(llm)
 
