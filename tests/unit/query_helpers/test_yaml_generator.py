9c0eb1e41746be6c1d3d9b5e5479f31c5e6111eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-11 18:21:28 +0100

    Better checks

--- tests/unit/query_helpers/test_yaml_generator.py
+++ tests/unit/query_helpers/test_yaml_generator.py
@@ -32,23 +32,46 @@ def test_yaml_generator(yaml_generator, monkeypatch):
 
 def test_yaml_generator_history_enabled(yaml_generator, monkeypatch):
     """Test the basic functionality of YAML generator when history is enabled."""
+    history_value = "conversation history"
 
-    ml = mock_llm_chain({"text": "default"})
+    def llmchain_param_check(self, *args, **kwargs):
+        """Modified version of __call__ special method for LLMChain mock."""
+        assert "inputs" in kwargs, "Missing keyword argument 'input'"
+        inputs = kwargs["inputs"]
+        assert "history" in inputs, f"Missing attribute 'history' in {inputs}"
+        assert inputs["history"] == history_value
+        return {"text": "default"}
+
+    # use mocked LLMChain
+    ml = mock_llm_chain(None)
     monkeypatch.setattr(ols.src.query_helpers.yaml_generator, "LLMChain", ml)
+    # modify __call__ special method with more checks
+    ml.__call__ = llmchain_param_check
+
     monkeypatch.setattr(
         ols.src.query_helpers.yaml_generator, "LLMLoader", mock_llm_loader()
     )
 
     # response will be constant because we mocked LLMChain and LLMLoader
-    response = yaml_generator.generate_yaml("1234", "the query", history="history")
+    response = yaml_generator.generate_yaml(
+        "1234", "the query", history="conversation history"
+    )
     assert response == "default"
 
 
 def test_yaml_generator_verbose_enabled(yaml_generator, monkeypatch):
     """Test the basic functionality of YAML generator when verbosity is enabled."""
 
+    def llmchain_param_check(self, *args, **kwargs):
+        """Modified version of __init__ special method for LLMChain mock."""
+        assert "verbose" in kwargs, f"Missing attribute 'verbose' in {kwargs}"
+
+    # use mocked LLMChain
     ml = mock_llm_chain({"text": "default"})
     monkeypatch.setattr(ols.src.query_helpers.yaml_generator, "LLMChain", ml)
+    # modify __init__ special method with more checks
+    ml.__init__ = llmchain_param_check
+
     monkeypatch.setattr(
         ols.src.query_helpers.yaml_generator, "LLMLoader", mock_llm_loader()
     )
