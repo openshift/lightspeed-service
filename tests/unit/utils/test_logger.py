7221f7422d91b44de1e6a010f73a359947ed4600
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-18 09:20:19 +0100

    Slightly better unit tests for logger

--- tests/unit/utils/test_logger.py
+++ tests/unit/utils/test_logger.py
@@ -17,7 +17,9 @@ def test_logger_debug_level(mock_load_dotenv, capsys):
     # check captured log output
     captured_out = capsys.readouterr().out
     assert "Debug message" in captured_out
-    captured_err = capsys.readouterr().out
+
+    # error output should be empty
+    captured_err = capsys.readouterr().err
     assert captured_err == ""
 
 
@@ -32,7 +34,9 @@ def test_logger_info_level(mock_load_dotenv, capsys):
     # the log message should not be captured due to log level
     captured_out = capsys.readouterr().out
     assert captured_out == ""
-    captured_err = capsys.readouterr().out
+
+    # error output should be empty as well
+    captured_err = capsys.readouterr().err
     assert captured_err == ""
 
 
@@ -45,8 +49,27 @@ def test_logger_show_message_flag(mock_load_dotenv, capsys):
 
     # check captured log output
     # the log message should not be captured due to log level
-    # but the message should be shown
     captured_out = capsys.readouterr().out
     assert "Set LOG_LEVEL environment variable (e.g., INFO, DEBUG)" in captured_out
-    captured_err = capsys.readouterr().out
+    assert "Debug message" not in captured_out
+
+    # error level should be empty
+    captured_err = capsys.readouterr().err
+    assert captured_err == ""
+
+
+@patch("dotenv.load_dotenv")
+@patch.dict(os.environ, {"LOG_LEVEL": "INFO"})
+def test_logger_error_messages(mock_load_dotenv, capsys):
+    """Test how logger log error messages."""
+    logger = Logger(logger_name="foo", log_level=logging.INFO)
+    logger.logger.error("Error message")
+
+    # check captured log output
+    # the log message should be captured
+    captured_out = capsys.readouterr().out
+    assert "ERROR: Error message" in captured_out
+
+    # error level should be empty
+    captured_err = capsys.readouterr().err
     assert captured_err == ""
