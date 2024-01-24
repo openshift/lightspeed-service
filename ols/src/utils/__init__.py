3fbddb18e707c76a612fe4c05e0b926d491a0f33
Asutosh Samal <asamal@redhat.com>
2024-01-24 11:14:07 +0530

    feat: Add token handler utility for context
    review comment fix

---
+++ ols/src/utils/__init__.py
@@ -0,0 +1,1 @@
+"""Utility classes and functions."""
---
+++ ols/src/utils/constants.py
@@ -0,0 +1,10 @@
+"""Constants used in business logic."""
+
+# TODO: Move this to config/env variables.
+TOKENIZER_MODEL = "cl100k_base"
+
+CONTEXT_WINDOW_LIMIT = 2000
+RESPONSE_WINDOW_LIMIT = 500
+MINIMUM_CONTEXT_LIMIT = 1
+
+SIMILARITY_SCORE_THRESHOLD = 0.5
