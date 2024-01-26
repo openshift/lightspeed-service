88edb37ece1cc64cbc3f73d2ccba5f14a86c1149
Ondrej Metelka <ometelka@redhat.com>
2024-01-26 12:54:14 +0100

    Fix logging tests

--- tests/unit/utils/test_logging.py
+++ tests/unit/utils/test_logging.py
@@ -7,27 +7,29 @@ from ols.utils.logging import LoggingConfig, configure_logging
 
 def test_configure_logging_shouldnt_log(caplog):
     """Test configure_logging function."""
+    caplog.set_level(logging.INFO)
     logging_config = LoggingConfig(
-        {"app_log_level": "debug"},
+        {"app_log_level": "info"},
     )
 
     configure_logging(logging_config)
     logger = logging.getLogger()
-    logger.info("level too high")
+    logger.debug("level too low")
 
     captured_out = caplog.text
-    assert "" in captured_out
+    assert captured_out == ""
 
 
 def test_configure_logging_should_log(caplog):
     """Test configure_logging function."""
+    caplog.set_level(logging.INFO)
     logging_config = LoggingConfig(
-        {"app_log_level": "warning"},
+        {"app_log_level": "info"},
     )
 
     configure_logging(logging_config)
     logger = logging.getLogger()
-    logger.warning("tadada")
+    logger.info("tadada")
 
     captured_out = caplog.text
     assert "tadada" in captured_out
