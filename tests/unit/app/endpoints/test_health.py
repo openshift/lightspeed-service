64a55cf3a4acadb51d46a816567b20d987bfd578
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-01-31 10:05:09 +0100

    Support for HEAD HTTP methods for liveness endpoints

--- tests/unit/app/endpoints/test_health.py
+++ tests/unit/app/endpoints/test_health.py
@@ -1,19 +1,40 @@
 """Unit tests for health endpoints handlers."""
 
-from ols.app.endpoints.health import liveness_probe, readiness_probe
+from ols.app.endpoints.health import (
+    liveness_probe_get_method,
+    liveness_probe_head_method,
+    readiness_probe_get_method,
+    readiness_probe_head_method,
+)
 
 
-def test_readiness_probe():
+def test_readiness_probe_get_method():
     """Test the readiness_probe function."""
     # the tested function returns constant right now
     # i.e. it does not depend on application state
-    response = readiness_probe()
+    response = readiness_probe_get_method()
     assert response == {"status": "1"}
 
 
-def test_liveness_probe():
+def test_liveness_probe_get_method():
     """Test the liveness_probe function."""
     # the tested function returns constant right now
     # i.e. it does not depend on application state
-    response = liveness_probe()
+    response = liveness_probe_get_method()
     assert response == {"status": "1"}
+
+
+def test_readiness_probe_head_method():
+    """Test the readiness_probe function."""
+    # the tested function returns constant right now
+    # i.e. it does not depend on application state
+    response = readiness_probe_head_method()
+    assert response is None
+
+
+def test_liveness_probe_head_method():
+    """Test the liveness_probe function."""
+    # the tested function returns constant right now
+    # i.e. it does not depend on application state
+    response = liveness_probe_head_method()
+    assert response is None
