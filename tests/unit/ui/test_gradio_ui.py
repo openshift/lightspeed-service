cd1022d6d9b33aaec1c8bf5a2344d7a0d5ce7a99
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-01 15:09:59 +0100

    More unit tests for Gradio UI

--- tests/unit/ui/test_gradio_ui.py
+++ tests/unit/ui/test_gradio_ui.py
@@ -1,10 +1,22 @@
 """Unit tests for gradioUI class."""
 
+import logging
 from unittest.mock import patch
 
 import requests
 
 from ols.src.ui.gradio_ui import GradioUI
+from ols.utils.logging import LoggingConfig, configure_logging
+
+
+def setup_logging(caplog):
+    """Set up logging and capturing log messsages."""
+    logging_config = LoggingConfig(
+        {"app_log_level": "info"},
+    )
+    configure_logging(logging_config)
+    logger = logging.getLogger("ols")
+    logger.handlers = [caplog.handler]  # add caplog handler to logger
 
 
 def test_gradio_ui_constructor():
@@ -18,8 +30,10 @@ def test_gradio_ui_constructor():
     assert ui.conversation_id == conversation_id
 
 
-def test_chat_ui_handler_ok_response():
+def test_chat_ui_handler_ok_response(caplog):
     """Test the UI handler for proper REST API response."""
+    setup_logging(caplog)
+
     ok_response = requests.Response()
     ok_response.status_code = requests.codes.ok
     ok_response.json = lambda: {"response": "this is response"}
@@ -28,6 +42,75 @@ def test_chat_ui_handler_ok_response():
         ui = GradioUI()
         ret = ui.chat_ui("prompt", None, False)
         assert ret == "this is response"
+
+    captured_out = caplog.text
+    assert "Using history: False" in captured_out
+    assert "Ignoring conversation history" in captured_out
+
+
+def test_chat_ui_handler_use_history_enabled(caplog):
+    """Test the UI handler for proper REST API response when history is enabled."""
+    setup_logging(caplog)
+
+    ok_response = requests.Response()
+    ok_response.status_code = requests.codes.ok
+    ok_response.json = lambda: {"response": "this is response"}
+
+    with patch("ols.src.ui.gradio_ui.requests.post", return_value=ok_response):
+        ui = GradioUI()
+        ret = ui.chat_ui("prompt", None, True)
+        assert ret == "this is response"
+
+    captured_out = caplog.text
+    assert "Using history: True" in captured_out
+    assert "Ignoring conversation history" not in captured_out
+
+
+def test_chat_ui_handler_use_with_conversation_id(caplog):
+    """Test the UI handler for proper REST API response when history is enabled."""
+    ok_response = requests.Response()
+    ok_response.status_code = requests.codes.ok
+    ok_response.json = lambda: {"response": "this is response"}
+
+    conversation_id = "01234567-89ab-cdef-0123-456789abcdef"
+    with patch("ols.src.ui.gradio_ui.requests.post", return_value=ok_response):
+        ui = GradioUI(conversation_id=conversation_id)
+        ret = ui.chat_ui("prompt", None, True)
+        assert ret == "this is response"
+
+    captured_out = caplog.text
+    assert "Ignoring conversation history" not in captured_out
+    assert f"Using conversation ID: {conversation_id}" in captured_out
+
+
+def test_chat_ui_handler_use_with_provider(caplog):
+    """Test the UI handler for proper REST API response when provider is setup."""
+    ok_response = requests.Response()
+    ok_response.status_code = requests.codes.ok
+    ok_response.json = lambda: {"response": "this is response"}
+
+    with patch("ols.src.ui.gradio_ui.requests.post", return_value=ok_response):
+        ui = GradioUI()
+        ret = ui.chat_ui("prompt", None, True, provider="PROVIDER")
+        assert ret == "this is response"
+
+    captured_out = caplog.text
+    assert "Using provider: PROVIDER" in captured_out
+
+
+def test_chat_ui_handler_use_with_model(caplog):
+    """Test the UI handler for proper REST API response when model is setup."""
+    ok_response = requests.Response()
+    ok_response.status_code = requests.codes.ok
+    ok_response.json = lambda: {"response": "this is response"}
+
+    with patch("ols.src.ui.gradio_ui.requests.post", return_value=ok_response):
+        ui = GradioUI()
+        ret = ui.chat_ui("prompt", None, True, model="MODEL")
+        assert ret == "this is response"
+
+    captured_out = caplog.text
+    assert "Using model: MODEL" in captured_out
 
 
 def test_chat_ui_handler_bad_http_code():
