af46b2029eba6194e33cd275d931307595471930
Ondrej Metelka <ometelka@redhat.com>
2024-02-16 09:55:15 +0100

    Make provider/model required inputs for LLMLoader

--- tests/unit/llms/test_llm_loader.py
+++ tests/unit/llms/test_llm_loader.py
@@ -1,17 +1,12 @@
 """Unit tests for LLMLoader class."""
 
-import sys
-from unittest.mock import MagicMock
-
 import pytest
 
 from ols import constants
-from ols.app.models.config import Config
+from ols.app.models.config import Config, LLMProviders
 from ols.src.llms.llm_loader import (
     LLMConfigurationError,
     LLMLoader,
-    MissingModelError,
-    MissingProviderError,
     ModelConfigInvalidError,
     ModelConfigMissingError,
     UnknownProviderError,
@@ -20,54 +15,23 @@ from ols.src.llms.llm_loader import (
 from ols.utils import config
 
 
-def setup():
-    """Provide environment for tests."""
-    # this function is called automatically by Pytest tool
-    # before unit tests are started
-    config.init_empty_config()
-
-    # the following modules should not be loaded during unit testing
-    # (these are not available on CI anyway)
-    mock_modules = [
-        "genai",
-        "genai.extensions.langchain",
-        "genai.text.generation",
-        "langchain.llms",
-        "ibm_watson_machine_learning.foundation_models",
-        "ibm_watson_machine_learning.foundation_models.extensions.langchain",
-        "ibm_watson_machine_learning.metanames",
-    ]
-    # make Python think that the modules are loaded already
-    for module in mock_modules:
-        sys.modules[module] = MagicMock()
-
-
 def test_errors_relationship():
     """Test the relationship between LLMConfigurationError and its subclasses."""
-    assert issubclass(MissingProviderError, LLMConfigurationError)
-    assert issubclass(MissingModelError, LLMConfigurationError)
     assert issubclass(UnknownProviderError, LLMConfigurationError)
     assert issubclass(UnsupportedProviderError, LLMConfigurationError)
     assert issubclass(ModelConfigMissingError, LLMConfigurationError)
     assert issubclass(ModelConfigInvalidError, LLMConfigurationError)
 
 
-def test_constructor_no_provider():
-    """Test that constructor checks for provider."""
-    with pytest.raises(MissingProviderError, match="Missing provider"):
-        LLMLoader(provider=None)
-
-
-def test_constructor_no_model():
-    """Test that constructor checks for model."""
-    with pytest.raises(MissingModelError, match="Missing model"):
-        LLMLoader(provider=constants.PROVIDER_BAM, model=None)
-
-
 def test_constructor_unknown_provider():
-    """Test how a provider missing from configuration is checked for."""
-    with pytest.raises(UnknownProviderError):
-        LLMLoader(provider="unknown-provider", model=constants.GRANITE_13B_CHAT_V1)
+    """Test raises when provider is unknown."""
+    providers = LLMProviders()  # no providers
+    config.init_empty_config()
+    config.config.llm_providers = providers
+
+    msg = "Provider 'unknown-provider' is not a valid provider"
+    with pytest.raises(UnknownProviderError, match=msg):
+        LLMLoader(provider="unknown-provider", model="some model")
 
 
 def test_constructor_unsupported_provider():
@@ -96,38 +60,33 @@ def test_constructor_unsupported_provider():
         LLMLoader(provider=test_provider, model=constants.GRANITE_13B_CHAT_V1)
 
 
-def test_constructor_when_missing_model_config():
+def test_constructor_unknown_model():
     """Test raise when model configuration is missing."""
-    test_provider = "test-provider"
-    test_model = "test-model"
-    config.config = Config(
-        {
-            "llm_providers": [
-                {"name": test_provider, "type": "bam", "models": [{"name": "foobar"}]}
-            ]
-        }
-    )
-    config.llm_config = config.config.llm_providers
+    providers = LLMProviders([{"name": "bam", "models": [{"name": "model"}]}])
+    config.init_empty_config()
+    config.config.llm_providers = providers
 
-    message = f"No configuration provided for model {test_model} under LLM provider {test_provider}"
+    message = "Model 'bla' is not a valid model for provider"
     with pytest.raises(ModelConfigMissingError, match=message):
-        LLMLoader(provider=test_provider, model=test_model)
+        LLMLoader(provider="bam", model="bla")
 
 
-providers = [
-    constants.PROVIDER_OPENAI,
-    constants.PROVIDER_BAM,
-]
+# TODO: As the LLMLoader is a subject of changes via OLS-233. Tests will
+# be delivered with the task.
+# Once changes are done, refactor also test bellow.
+#
+# providers = [
+#     constants.PROVIDER_OPENAI,
+#     constants.PROVIDER_BAM,
+# ]
+# @pytest.mark.parametrize("provider", providers)
+# def test_constructor_no_keys(provider):
+#     """Test raise when keys are missing."""
+#     test_model = constants.TEI_EMBEDDING_MODEL
+#     config.config = Config(
+#         {"llm_providers": [{"name": provider, "models": [{"name": test_model}]}]}
+#     )
+#     config.llm_config = config.config.llm_providers
 
-
-@pytest.mark.parametrize("provider", providers)
-def test_constructor_no_keys(provider):
-    """Test raise when keys are missing."""
-    test_model = constants.TEI_EMBEDDING_MODEL
-    config.config = Config(
-        {"llm_providers": [{"name": provider, "models": [{"name": test_model}]}]}
-    )
-    config.llm_config = config.config.llm_providers
-
-    with pytest.raises(ValueError, match="api_key"):
-        LLMLoader(provider=provider, model=test_model)
+#     with pytest.raises(ValueError, match="api_key"):
+#         LLMLoader(provider=provider, model=test_model)
--- tests/unit/query_helpers/test_query_helper.py
+++ tests/unit/query_helpers/test_query_helper.py
@@ -1,26 +1,27 @@
 """Unit tests for query helper class."""
 
+from ols.src.llms.llm_loader import LLMLoader
 from ols.src.query_helpers import QueryHelper
 from ols.utils import config
 
 
-class TestQueryHelper:
-    """Test the query helper class."""
+def test_defaults_used():
+    """Test that the defaults are used when no inputs are provided."""
+    config.init_config("tests/config/valid_config.yaml")
 
-    def test_defaults_used(self):
-        """Test that the defaults are used when no inputs are provided."""
-        config.init_config("tests/config/valid_config.yaml")
+    qh = QueryHelper()
 
-        qh = QueryHelper()
+    assert qh.provider == config.ols_config.default_provider
+    assert qh.model == config.ols_config.default_model
+    assert qh.llm_loader == LLMLoader
+    assert qh.llm_params == {}
 
-        assert qh.provider == config.ols_config.default_provider
-        assert qh.model == config.ols_config.default_model
 
-    def test_inputs_are_used(self):
-        """Test that the inputs are used when provided."""
-        test_provider = "test_provider"
-        test_model = "test_model"
-        qh = QueryHelper(provider=test_provider, model=test_model)
+def test_inputs_are_used():
+    """Test that the inputs are used when provided."""
+    test_provider = "test_provider"
+    test_model = "test_model"
+    qh = QueryHelper(provider=test_provider, model=test_model)
 
-        assert qh.provider == test_provider
-        assert qh.model == test_model
+    assert qh.provider == test_provider
+    assert qh.model == test_model
