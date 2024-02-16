42c25fb60a0e20e71d6df3fcd9cde5c3363ede59
bparees <bparees@redhat.com>
2024-02-16 10:20:52 -0500

    Bump the gradio timeout

--- ols/src/ui/gradio_ui.py
+++ ols/src/ui/gradio_ui.py
@@ -71,9 +71,9 @@ class GradioUI:
         json_data = json.dumps(data)
 
         try:
-            # Make the HTTP POST request, wait for response with 30 seconds timeout
+            # Make the HTTP POST request, wait for response with 90 seconds timeout
             response = requests.post(
-                self.ols_url, headers=headers, data=json_data, timeout=30
+                self.ols_url, headers=headers, data=json_data, timeout=90
             )
 
             # Check if the request was successful (status code 200)
