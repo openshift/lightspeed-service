8117e6e024bf80844b6490a366615f3d99cac272
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-02-21 16:26:38 +0100

    Disable Gradio telemetry as soon as possible

--- runner.py
+++ runner.py
@@ -1,6 +1,12 @@
 """Script that is able to start Uvicorn-based REST API service."""
 
+import os
+
 import uvicorn
 
 if __name__ == "__main__":
+    # TODO syedriko consolidate this env var into a central location as per OLS-345.
+    # disable Gradio analytics, which calls home to https://api.gradio.app
+    os.environ["GRADIO_ANALYTICS_ENABLED"] = "false"
+
     uvicorn.run("ols.app.main:app", host="127.0.0.1", port=8080, reload=True)
