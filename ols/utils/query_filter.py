f3ce284a759613734e5667be563ef75bf375b677
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-08 12:04:02 +0100

    Don't shadow filter builtin

--- ols/utils/query_filter.py
+++ ols/utils/query_filter.py
@@ -21,13 +21,13 @@ class QueryFilter:
         logger.debug(f"Query filters : {config.ols_config.query_filters}")
         if not config.ols_config.query_filters:
             return
-        for filter in config.ols_config.query_filters:
-            pattern = re.compile(str(filter.pattern))
+        for query_filter in config.ols_config.query_filters:
+            pattern = re.compile(str(query_filter.pattern))
             regex_filters.append(
                 RegexFilter(
                     pattern=pattern,
-                    name=filter.name,
-                    replace_with=filter.replace_with,
+                    name=query_filter.name,
+                    replace_with=query_filter.replace_with,
                 )
             )
         self.regex_filters = regex_filters
@@ -35,8 +35,8 @@ class QueryFilter:
     def redact_query(self, conversation_id: str, query: str) -> str:
         """Redact the query using regex built."""
         logger.debug(f"Redacting conversation {conversation_id} query: {query}")
-        for filter in self.regex_filters:
-            query, count = filter.pattern.subn(filter.replace_with, query)
-            logger.debug(f"Replaced: {count} matched with filter : {filter.name}")
+        for query_filter in self.regex_filters:
+            query, count = query_filter.pattern.subn(query_filter.replace_with, query)
+            logger.debug(f"Replaced: {count} matched with filter : {query_filter.name}")
         logger.debug(f"Redacted conversation {conversation_id} query: {query}")
         return query
