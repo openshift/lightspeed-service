c3918eb7bea2d37d7a3612df5474a209ee4930ab
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-03-26 10:26:17 +0100

    Provider parameters check

--- tests/unit/llms/providers/test_openai.py
+++ tests/unit/llms/providers/test_openai.py
@@ -45,15 +45,61 @@ def test_params_handling(provider_config):
     """Test that not allowed parameters are removed before model init."""
     config.init_empty_config()  # needed for checking the config.dev_config.llm_params
 
-    # these two parameters should be removed before model init
+    # first three parameters should be removed before model init
+    # rest need to stay
     params = {
+        "unknown_parameter": "foo",
         "min_new_tokens": 1,
         "max_new_tokens": 10,
+        "temperature": 0.3,
+        "verbose": True,
     }
 
     openai = OpenAI(model="uber-model", params=params, provider_config=provider_config)
     llm = openai.load()
     assert isinstance(llm, ChatOpenAI)
     assert openai.default_params
-    assert "min_new_tokens" not in openai.default_params
-    assert "max_new_tokens" not in openai.default_params
+    assert openai.params
+
+    # known parameters should be there
+    assert "temperature" in openai.params
+    assert "verbose" in openai.params
+    assert openai.params["temperature"] == 0.3
+    assert openai.params["verbose"] is True
+
+    # unknown parameters should be filtered out
+    assert "min_new_tokens" not in openai.params
+    assert "max_new_tokens" not in openai.params
+    assert "unknown_parameter" not in openai.params
+
+
+def test_none_params_handling(provider_config):
+    """Test that not allowed parameters are removed before model init."""
+    config.init_empty_config()  # needed for checking the config.dev_config.llm_params
+
+    # first three parameters should be removed before model init
+    # rest need to stay
+    params = {
+        "unknown_parameter": None,
+        "min_new_tokens": None,
+        "max_new_tokens": None,
+        "organization": None,
+        "cache": None,
+    }
+
+    openai = OpenAI(model="uber-model", params=params, provider_config=provider_config)
+    llm = openai.load()
+    assert isinstance(llm, ChatOpenAI)
+    assert openai.default_params
+    assert openai.params
+
+    # known parameters should be there, with None values
+    assert "cache" in openai.params
+    assert "organization" in openai.params
+    assert openai.params["cache"] is None
+    assert openai.params["organization"] is None
+
+    # unknown parameters should be filtered out
+    assert "min_new_tokens" not in openai.params
+    assert "max_new_tokens" not in openai.params
+    assert "unknown_parameter" not in openai.params
