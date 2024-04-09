ae644679f487c95b50b6b0772d294d4040986779
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-09 14:23:46 +0200

    Better error messages for auth. endpoint

--- ols/app/endpoints/authorized.py
+++ ols/app/endpoints/authorized.py
@@ -5,15 +5,39 @@ import logging
 
 from fastapi import APIRouter, Request
 
-from ols.app.models.models import AuthorizationResponse
+from ols.app.models.models import (
+    AuthorizationResponse,
+    ErrorResponse,
+    ForbiddenResponse,
+    UnauthorizedResponse,
+)
 from ols.utils.auth_dependency import AuthDependency
 
 logger = logging.getLogger(__name__)
 router = APIRouter(tags=["authorized"])
 auth_dependency = AuthDependency(virtual_path="/ols-access")
 
+authorized_responses = {
+    200: {
+        "description": "The user is logged-in and authorized to access OLS",
+        "model": AuthorizationResponse,
+    },
+    401: {
+        "description": "Missing or invalid credentials provided by client",
+        "model": UnauthorizedResponse,
+    },
+    403: {
+        "description": "User is not authorized",
+        "model": ForbiddenResponse,
+    },
+    500: {
+        "description": "Unexpected error during token review",
+        "model": ErrorResponse,
+    },
+}
 
-@router.post("/authorized")
+
+@router.post("/authorized", responses=authorized_responses)
 def is_user_authorized(request: Request) -> AuthorizationResponse:
     """Validate if the logged-in user is authorized to access OLS.
 
