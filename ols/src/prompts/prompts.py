d0886b3f1040af4b9f3b1b9a6c4f158d82f9b28a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-04-10 10:14:00 +0200

    Add TODO for OLS-503: Fine tune system prompt

--- ols/src/prompts/prompts.py
+++ ols/src/prompts/prompts.py
@@ -5,7 +5,7 @@
 
 from ols.constants import SUBJECT_ALLOWED, SUBJECT_REJECTED
 
-# TODO: Fine-tune system prompt
+# TODO: OLS-503 Fine tune system prompt
 QUERY_SYSTEM_PROMPT = """You are an assistant for question-answering tasks \
 related to the openshift and kubernetes container orchestration platforms. \
 """
