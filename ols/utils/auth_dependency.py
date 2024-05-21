fff8e7d11d01392e8c2afb953801ad22cb127fbf
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-05-21 18:04:01 +0200

    Added TODO for OLS-648

--- ols/utils/auth_dependency.py
+++ ols/utils/auth_dependency.py
@@ -1,6 +1,7 @@
 """Manage authentication flow for FastAPI endpoints with K8S/OCP."""
 
 import logging
+from pathlib import Path
 from typing import Optional, Self
 
 import kubernetes.client
@@ -37,6 +38,7 @@ class K8sClientSingleton:
             configuration = kubernetes.client.Configuration()
 
             try:
+                # TODO: OLS-648 Broken logic in check if k8s_cluster_api is configured
                 if config.ols_config.authentication_config.k8s_cluster_api not in {
                     None,
                     "",
@@ -69,6 +71,7 @@ class K8sClientSingleton:
                                   and no override token was provided: {e}"
                             )
 
+                # TODO: OLS-648 Broken logic in check if k8s_cluster_api is configured
                 configuration.host = (
                     config.ols_config.authentication_config.k8s_cluster_api
                     if config.ols_config.authentication_config.k8s_cluster_api
@@ -81,7 +84,7 @@ class K8sClientSingleton:
                 configuration.ssl_ca_cert = (
                     config.ols_config.authentication_config.k8s_ca_cert_path
                     if config.ols_config.authentication_config.k8s_ca_cert_path
-                    not in {None, ""}
+                    not in {None, Path("")}
                     else configuration.ssl_ca_cert
                 )
                 api_client = kubernetes.client.ApiClient(configuration)
