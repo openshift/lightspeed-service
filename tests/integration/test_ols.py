d9863eea707892a7bdff03da6747ca204970c5b2
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-18 14:56:14 +0200

    X

--- tests/integration/test_ols.py
+++ tests/integration/test_ols.py
@@ -50,3 +50,69 @@ def test_post_question_on_unexpected_payload(_setup):
             }
         ],
     }
+
+
+def test_post_question_without_payload(_setup):
+    """Check the REST API /v1/query with POST HTTP method when no payload is posted."""
+    # perform POST request without any payload
+    response = client.post("/v1/query")
+    assert response.status_code == requests.codes.unprocessable
+
+    # check the response payload
+    json = response.json()
+    assert "detail" in json, "Missing 'detail' node in response payload"
+    detail = json["detail"][0]
+    assert detail["input"] is None
+    assert "Field required" in detail["msg"]
+
+
+def test_post_question_on_invalid_question(_setup):
+    """Check the REST API /v1/query with POST HTTP method for invalid question."""
+    # let's pretend the question is invalid without even asking LLM
+    with patch(
+        "ols.app.endpoints.ols.QuestionValidator.validate_question", return_value=False
+    ):
+        conversation_id = suid.get_suid()
+        response = client.post(
+            "/v1/query",
+            json={"conversation_id": conversation_id, "query": "test query"},
+        )
+        assert response.status_code == requests.codes.ok
+
+        expected_json = {
+            "conversation_id": conversation_id,
+            "response": constants.INVALID_QUERY_RESP,
+            "referenced_documents": [],
+            "truncated": False,
+        }
+        assert response.json() == expected_json
+
+
+def test_post_question_on_generic_response_type_summarize_error(_setup):
+    """Check the REST API /v1/query with POST HTTP method when generic response type is returned."""
+    # let's pretend the question is valid and generic one
+    answer = constants.SUBJECT_ALLOWED
+    with (
+        patch(
+            "ols.app.endpoints.ols.QuestionValidator.validate_question",
+            return_value=answer,
+        ),
+        patch(
+            "ols.app.endpoints.ols.DocsSummarizer.summarize",
+            side_effect=Exception("summarizer error"),
+        ),
+    ):
+        conversation_id = suid.get_suid()
+        response = client.post(
+            "/v1/query",
+            json={"conversation_id": conversation_id, "query": "test query"},
+        )
+        assert response.status_code == requests.codes.internal_server_error
+        expected_json = {
+            "detail": {
+                "cause": "summarizer error",
+                "response": "Error while obtaining answer for user question",
+            }
+        }
+
+        assert response.json() == expected_json
