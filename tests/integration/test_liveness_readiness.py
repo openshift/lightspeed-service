45c87a03323a411f69816b382feffdd2a3077647
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-18 11:33:35 +0200

    X

---
+++ tests/integration/test_liveness_readiness.py
@@ -0,0 +1,64 @@
+"""Integration tests for /livenss and /readiness REST API endpoints."""
+
+import os
+from unittest.mock import patch
+
+import pytest
+import requests
+from fastapi.testclient import TestClient
+
+from ols import config
+
+
+# we need to patch the config file path to point to the test
+# config file before we import anything from main.py
+@pytest.fixture(scope="function", autouse=True)
+@patch.dict(
+    os.environ, {"OLS_CONFIG_FILE": "tests/config/config_for_integration_tests.yaml"}
+)
+def _setup():
+    """Setups the test client."""
+    global client
+    config.reload_from_yaml_file("tests/config/config_for_integration_tests.yaml")
+
+    # app.main need to be imported after the configuration is read
+    from ols.app.main import app
+
+    client = TestClient(app)
+
+
+def test_liveness():
+    """Test handler for /liveness REST API endpoint."""
+    response = client.get("/liveness")
+    assert response.status_code == requests.codes.ok
+    assert response.json() == {"alive": True}
+
+
+def test_readiness():
+    """Test handler for /readiness REST API endpoint."""
+    # index is not ready
+    with (
+        patch("ols.app.endpoints.health.llm_is_ready", return_value=True),
+        patch("ols.app.endpoints.health.index_is_ready", return_value=False),
+    ):
+        response = client.get("/readiness")
+        assert response.status_code == requests.codes.ok
+        assert response.json() == {"ready": False, "reason": "index is not ready"}
+
+    # index is ready, LLM is not ready
+    with (
+        patch("ols.app.endpoints.health.llm_is_ready", return_value=False),
+        patch("ols.app.endpoints.health.index_is_ready", return_value=True),
+    ):
+        response = client.get("/readiness")
+        assert response.status_code == requests.codes.ok
+        assert response.json() == {"ready": False, "reason": "LLM is not ready"}
+
+    # both index and LLM are ready
+    with (
+        patch("ols.app.endpoints.health.llm_is_ready", return_value=True),
+        patch("ols.app.endpoints.health.index_is_ready", return_value=True),
+    ):
+        response = client.get("/readiness")
+        assert response.status_code == requests.codes.ok
+        assert response.json() == {"ready": True, "reason": "service is ready"}
---
+++ tests/integration/test_redis.py
@@ -0,0 +1,66 @@
+"""Integration tests for real Redis behaviour."""
+
+import pytest
+
+from ols.app.models.config import RedisConfig
+from ols.app.models.models import CacheEntry
+from ols.src.cache.redis_cache import RedisCache
+
+user_id = "00000000-0000-0000-0000-000000000001"
+conversation_id = "00000000-0000-0000-0000-000000000002"
+
+
+@pytest.mark.redis()
+def setup():
+    """Setups the Redis client."""
+    global redis_cache
+
+    # please note that the setup expect Redis running locally on default port
+    redis_config = RedisConfig(
+        {
+            "host": "localhost",
+            "port": 6379,
+            "max_memory": "100mb",
+            "max_memory_policy": "allkeys-lru",
+            "retry_on_error": "false",
+            "retry_on_timeout": "false",
+        }
+    )
+
+    redis_cache = RedisCache(redis_config)
+
+
+@pytest.mark.redis()
+def test_conversation_in_redis():
+    """Check the elementary GET operation and insert_or_append operation."""
+    # make sure the cache is empty
+    redis_cache.redis_client.delete(user_id + ":" + conversation_id)
+
+    # the initial value should be empty
+    retrieved = redis_cache.get(user_id, conversation_id)
+    assert retrieved is None
+
+    # insert some conversation
+    cache_entry = CacheEntry(query="First human message", response="First AI response")
+    redis_cache.insert_or_append(user_id, conversation_id, cache_entry)
+
+    # check what is stored in conversation cache
+    retrieved = redis_cache.get(user_id, conversation_id)
+    assert retrieved is not None
+
+    # just the initial cache_entry should be stored
+    assert retrieved == cache_entry
+
+    # append more conversation
+    cache_entry_2 = CacheEntry(
+        query="Second human message", response="Second AI response"
+    )
+
+    redis_cache.insert_or_append(user_id, conversation_id, cache_entry_2)
+
+    # check what is stored in conversation cache
+    retrieved = redis_cache.get(user_id, conversation_id)
+    assert retrieved is not None
+
+    # now both conversations should be stored
+    assert retrieved == [cache_entry, cache_entry_2]
