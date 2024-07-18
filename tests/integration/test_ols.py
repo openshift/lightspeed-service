c1d7456389a312bc1b06be3135ba99076e2656ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-07-18 16:49:58 +0200

    X

--- tests/integration/test_ols.py
+++ tests/integration/test_ols.py
@@ -431,3 +431,85 @@ def test_post_query_for_conversation_history(_setup) -> None:
                 },
                 config={"callbacks": [token_counter.return_value]},
             )
+
+
+def test_post_question_without_attachments(_setup) -> None:
+    """Check the REST API /v1/query with POST HTTP method without attachments."""
+    answer = constants.SUBJECT_ALLOWED
+    query_passed = None
+
+    def validate_question(conversation_id, query):
+        """Closure called indirectly to validate the question."""
+        nonlocal query_passed
+        query_passed = query
+        return answer
+
+    with patch(
+        "ols.app.endpoints.ols.QuestionValidator.validate_question",
+        side_effect=validate_question,
+    ):
+        from tests.mock_classes.mock_langchain_interface import mock_langchain_interface
+
+        ml = mock_langchain_interface("test response")
+        with (
+            patch(
+                "ols.src.query_helpers.docs_summarizer.LLMChain",
+                new=mock_llm_chain(None),
+            ),
+            patch(
+                "ols.src.query_helpers.query_helper.load_llm",
+                new=mock_llm_loader(ml()),
+            ),
+        ):
+            conversation_id = suid.get_suid()
+            response = client.post(
+                "/v1/query",
+                json={
+                    "conversation_id": conversation_id,
+                    "query": "test query",
+                },
+            )
+            assert response.status_code == requests.codes.ok
+    assert query_passed == "test query"
+
+
+@pytest.mark.attachment()
+def test_post_question_with_empty_list_of_attachments(_setup) -> None:
+    """Check the REST API /v1/query with POST HTTP method with empty list of attachments."""
+    answer = constants.SUBJECT_ALLOWED
+    query_passed = None
+
+    def validate_question(conversation_id, query):
+        """Closure called indirectly to validate the question."""
+        nonlocal query_passed
+        query_passed = query
+        return answer
+
+    with patch(
+        "ols.app.endpoints.ols.QuestionValidator.validate_question",
+        side_effect=validate_question,
+    ):
+        from tests.mock_classes.mock_langchain_interface import mock_langchain_interface
+
+        ml = mock_langchain_interface("test response")
+        with (
+            patch(
+                "ols.src.query_helpers.docs_summarizer.LLMChain",
+                new=mock_llm_chain(None),
+            ),
+            patch(
+                "ols.src.query_helpers.query_helper.load_llm",
+                new=mock_llm_loader(ml()),
+            ),
+        ):
+            conversation_id = suid.get_suid()
+            response = client.post(
+                "/v1/query",
+                json={
+                    "conversation_id": conversation_id,
+                    "query": "test query",
+                    "attachments": [],
+                },
+            )
+            assert response.status_code == requests.codes.ok
+    assert query_passed == "test query"
