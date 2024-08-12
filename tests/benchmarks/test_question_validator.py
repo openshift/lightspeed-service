0948769c96a8c1bb3371ebde793d01a41bde0725
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-08-12 09:05:29 +0200

    More benchmarks for QuestionValidator

--- tests/benchmarks/test_question_validator.py
+++ tests/benchmarks/test_question_validator.py
@@ -10,8 +10,8 @@ from tests.mock_classes.mock_llm_loader import mock_llm_loader
 
 
 @patch("ols.src.query_helpers.question_validator.LLMChain", new=mock_llm_chain(None))
-def test_validate_question_llm_loader(benchmark):
-    """Benchmarks the method QuestionValidator.validate_question."""
+def perform_question_validation_benchmark(benchmark, question):
+    """Prepare all mocks and the call the QuestionValidator.validate_question method."""
     # it is needed to initialize configuration in order to be able
     # to construct QuestionValidator instance
     config.reload_from_yaml_file("tests/config/valid_config.yaml")
@@ -31,5 +31,34 @@ def test_validate_question_llm_loader(benchmark):
     benchmark(
         question_validator.validate_question,
         "123e4567-e89b-12d3-a456-426614174000",
-        "query",
+        question,
     )
+
+
+def test_validate_question_short_question(benchmark):
+    """Benchmarks the method QuestionValidator.validate_question for short question."""
+    question = "question"
+    perform_question_validation_benchmark(benchmark, question)
+
+
+def test_validate_question_medium_question(benchmark):
+    """Benchmarks the method QuestionValidator.validate_question for medium-sized question."""
+    question = (
+        "Suppose I start with a newly deployed OpenShift 4.15 cluster, what do I need "
+        "to do to send logs from the cluster to AWS CloudWatch? "
+        "Is it even possible to do so?"
+    )
+    perform_question_validation_benchmark(benchmark, question)
+
+
+def test_validate_question_long_question(benchmark):
+    """Benchmarks the method QuestionValidator.validate_question for long question."""
+    question = (
+        "Suppose I start with a newly deployed OpenShift 4.15 cluster, what do I need "
+        "to do to send logs from the cluster to AWS CloudWatch? "
+        "Is it even possible to do so?"
+    )
+    # make sure we don't exceed context window size there
+    # it is set to just 450 tokens in the configuration file
+    question *= 8
+    perform_question_validation_benchmark(benchmark, question)
