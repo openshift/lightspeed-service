7df0fb0e2b7d70c5200c1ff1d29233957b7888cd
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-05 11:24:10 +0200

    Refactor: constant name

--- tests/integration/test_redis.py
+++ tests/integration/test_redis.py
@@ -6,8 +6,8 @@ from ols.app.models.config import RedisConfig
 from ols.app.models.models import CacheEntry
 from ols.src.cache.redis_cache import RedisCache
 
-user_id = "00000000-0000-0000-0000-000000000001"
-conversation_id = "00000000-0000-0000-0000-000000000002"
+USER_ID = "00000000-0000-0000-0000-000000000001"
+CONVERSATION_ID = "00000000-0000-0000-0000-000000000002"
 
 
 @pytest.mark.redis()
@@ -34,18 +34,18 @@ def setup():
 def test_conversation_in_redis():
     """Check the elementary GET operation and insert_or_append operation."""
     # make sure the cache is empty
-    redis_cache.redis_client.delete(user_id + ":" + conversation_id)
+    redis_cache.redis_client.delete(USER_ID + ":" + CONVERSATION_ID)
 
     # the initial value should be empty
-    retrieved = redis_cache.get(user_id, conversation_id)
+    retrieved = redis_cache.get(USER_ID, CONVERSATION_ID)
     assert retrieved is None
 
     # insert some conversation
     cache_entry = CacheEntry(query="First human message", response="First AI response")
-    redis_cache.insert_or_append(user_id, conversation_id, cache_entry)
+    redis_cache.insert_or_append(USER_ID, CONVERSATION_ID, cache_entry)
 
     # check what is stored in conversation cache
-    retrieved = redis_cache.get(user_id, conversation_id)
+    retrieved = redis_cache.get(USER_ID, CONVERSATION_ID)
     assert retrieved is not None
 
     # just the initial cache_entry should be stored
@@ -56,10 +56,10 @@ def test_conversation_in_redis():
         query="Second human message", response="Second AI response"
     )
 
-    redis_cache.insert_or_append(user_id, conversation_id, cache_entry_2)
+    redis_cache.insert_or_append(USER_ID, CONVERSATION_ID, cache_entry_2)
 
     # check what is stored in conversation cache
-    retrieved = redis_cache.get(user_id, conversation_id)
+    retrieved = redis_cache.get(USER_ID, CONVERSATION_ID)
     assert retrieved is not None
 
     # now both conversations should be stored
