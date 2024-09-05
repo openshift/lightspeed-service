14e2903ed80bb8a8cb64256946bafc50b268b01b
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-05 10:09:12 +0200

    Use class pattern in pattern matching

--- ols/utils/errors_parsing.py
+++ ols/utils/errors_parsing.py
@@ -48,10 +48,12 @@ def parse_watsonx_error(e: ApiRequestFailure) -> tuple[int, str, str]:
 
 def parse_generic_llm_error(e: Exception) -> tuple[int, str, str]:
     """Try to parse generic LLM error."""
-    if isinstance(e, BadRequestError):
-        return parse_openai_error(e)
-    elif isinstance(e, ApiResponseException):
-        return parse_bam_error(e)
-    elif isinstance(e, ApiRequestFailure):
-        return parse_watsonx_error(e)
-    return DEFAULT_STATUS_CODE, DEFAULT_ERROR_MESSAGE, str(e)
+    match e:
+        case BadRequestError():
+            return parse_openai_error(e)
+        case ApiResponseException():
+            return parse_bam_error(e)
+        case ApiRequestFailure():
+            return parse_watsonx_error(e)
+        case _:
+            return DEFAULT_STATUS_CODE, DEFAULT_ERROR_MESSAGE, str(e)
