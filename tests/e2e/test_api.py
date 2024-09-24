230ce0a647c61252109bd1905beaf4ffd2453a7d
Haoyu Sun <hasun@redhat.com>
2024-09-24 15:15:58 +0200

    e2e test: add ImageDigestMirrorSet to allow using bundle from unreleased images
    Signed-off-by: Haoyu Sun <hasun@redhat.com>

--- tests/e2e/test_api.py
+++ tests/e2e/test_api.py
@@ -84,6 +84,13 @@ def install_ols() -> tuple[str, str, str]:
     token = cluster_utils.get_token_for("test-user")
     metrics_token = cluster_utils.get_token_for("metrics-test-user")
     print("created test service account users")
+    
+    # install the ImageDigestMirrorSet to mirror images from "registry.redhat.io/openshift-lightspeed-beta" 
+    # to "quay.io/redhat-user-workloads/crt-nshift-lightspeed-tenant/ols"
+    cluster_utils.run_oc(
+        ["create", "-f", "tests/config/operator_install/imagedigestmirrorset.yaml"],
+        ignore_existing_resource=True,
+    )
 
     # install the operator catalog
     cluster_utils.run_oc(
