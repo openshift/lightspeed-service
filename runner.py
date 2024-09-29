9c0237c036416b922e8a517a3f096d709ef96d45
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-09-29 19:04:09 +0200

    Better import

--- runner.py
+++ runner.py
@@ -12,7 +12,7 @@ import uvicorn
 from cryptography import x509
 
 import ols.app.models.config as config_model
-import ols.constants as constants
+from ols import constants
 from ols.utils.auth_dependency import K8sClientSingleton
 from ols.utils.logging import configure_logging
 
