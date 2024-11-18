349e003d2a1daa8a32658853bcf674cbaf30622a
Pavel Tisnovsky <ptisnovs@redhat.com>
2024-11-18 21:09:46 +0100

    Refactor runner.py - SSL-related utility functions

--- runner.py
+++ runner.py
@@ -14,7 +14,7 @@ from cryptography import x509
 import ols.app.models.config as config_model
 from ols import constants
 from ols.src.auth.auth import use_k8s_auth
-from ols.utils import tls
+from ols.utils import ssl
 from ols.utils.logging import configure_logging
 
 
@@ -106,39 +106,6 @@ def generate_certificates_file(
         add_ca_to_certificates_store(logger, certificate_path, destination_file)
 
 
-def get_ssl_version(sec_profile):
-    """Get SSL version to be used. It can be configured in tls_security_profile section."""
-    # if security profile is not set, use default SSL version
-    # as specified in SSL library
-    if sec_profile is None or sec_profile.profile_type is None:
-        logger.info("Using default SSL version: %s", constants.DEFAULT_SSL_VERSION)
-        return constants.DEFAULT_SSL_VERSION
-
-    # security profile is set -> we need to retrieve SSL version and list of allowed ciphers
-    min_tls_version = tls.min_tls_version(
-        sec_profile.min_tls_version, sec_profile.profile_type
-    )
-    logger.info("min TLS version: %s", min_tls_version)
-
-    ssl_version = tls.ssl_tls_version(min_tls_version)
-    logger.info("Using SSL version: %s", ssl_version)
-    return ssl_version
-
-
-def get_ciphers(sec_profile):
-    """Get allowed ciphers to be used. It can be configured in tls_security_profile section."""
-    # if security profile is not set, use default ciphers
-    # as specified in SSL library
-    if sec_profile is None or sec_profile.profile_type is None:
-        logger.info("Allowing default ciphers: %s", constants.DEFAULT_SSL_CIPHERS)
-        return constants.DEFAULT_SSL_CIPHERS
-
-    # security profile is set -> we need to retrieve ciphers to be allowed
-    ciphers = tls.ciphers_as_string(sec_profile.ciphers, sec_profile.profile_type)
-    logger.info("Allowing following ciphers: %s", ciphers)
-    return ciphers
-
-
 def start_uvicorn():
     """Start Uvicorn-based REST API service."""
     # use workers=1 so config loaded can be accessed from other modules
@@ -159,8 +126,8 @@ def start_uvicorn():
     # when TLS security profile is not specified, default values will be used
     # that default values are based on default SSL package settings
     sec_profile = config.ols_config.tls_security_profile
-    ssl_version = get_ssl_version(sec_profile)
-    ssl_ciphers = get_ciphers(sec_profile)
+    ssl_version = ssl.get_ssl_version(sec_profile)
+    ssl_ciphers = ssl.get_ciphers(sec_profile)
 
     uvicorn.run(
         "ols.app.main:app",
