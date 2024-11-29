41f12450ed36a632d13abb4154f76cf173ab7421
jfula <jfula@redhat.com>
2024-11-29 14:06:46 +0100

    Adding a script file for periodic and removing model evaluation from precommit tests

--- tests/scripts/test-e2e-cluster-periodics.sh
+++ tests/scripts/test-e2e-cluster-periodics.sh
@@ -45,7 +45,7 @@ function run_suites() {
   run_suite "openai" "not model_evaluation and not azure_entra_id" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE"
   (( rc = rc || $? ))
 
-  run_suite "watsonx" "not model_evaluation and not azure_entra_id" "watsonx" "$WATSONX_PROVIDER_KEY_PATH" "ibm/granite-13b-chat-v2" "$OLS_IMAGE"
+  run_suite "watsonx" " not azure_entra_id" "watsonx" "$WATSONX_PROVIDER_KEY_PATH" "ibm/granite-13b-chat-v2" "$OLS_IMAGE"
   (( rc = rc || $? ))
 
   # smoke tests for RHOAI VLLM-compatible provider
