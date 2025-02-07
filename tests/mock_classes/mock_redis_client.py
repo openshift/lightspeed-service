7dcd3a4aa14f5f4fece932b1ebc2855867ac5dfd
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-02-07 12:09:08 +0100

    Updated tests accordingly

--- tests/mock_classes/mock_redis_client.py
+++ tests/mock_classes/mock_redis_client.py
@@ -1,5 +1,7 @@
 """Mock for StrictRedis client."""
 
+import fnmatch
+
 
 class MockRedisClient:
     """Mock for StrictRedis client.
@@ -41,3 +43,22 @@ class MockRedisClient:
         assert isinstance(value, (str, bytes, int, float))
 
         self.cache[key] = value
+
+    def delete(self, key):
+        """Return item from cache (implementation of DELETE command)."""
+        # real Redis accepts keys as strings only
+        assert isinstance(key, str)
+
+        if key in self.cache:
+            del self.cache[key]
+            return True  # successfuly deleted, return True
+        return False  # Key did not exist, return False
+
+    def keys(self, pattern):
+        """List keys matching a given pattern (implementation of KEYS command)."""
+        # real Redis accepts patterns as strings only
+        assert isinstance(pattern, str)
+
+        # Use fnmatch to match keys against the pattern
+        matching_keys = [key for key in self.cache if fnmatch.fnmatch(key, pattern)]
+        return matching_keys
