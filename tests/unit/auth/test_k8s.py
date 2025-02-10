07bc585ddba1903934e0974709e48d5cb8ddba6e
Ondrej Metelka <ometelka@redhat.com>
2025-02-10 08:57:53 +0100

    Forward user's token

--- tests/unit/auth/test_k8s.py
+++ tests/unit/auth/test_k8s.py
@@ -60,12 +60,13 @@ async def test_auth_dependency_valid_token(mock_authz_api, mock_authn_api):
         scope={"type": "http", "headers": [(b"authorization", b"Bearer valid-token")]}
     )
 
-    user_uid, username, skip_user_id_check = await auth_dependency(request)
+    user_uid, username, skip_user_id_check, token = await auth_dependency(request)
 
     # Check if the correct user info has been returned
     assert user_uid == "valid-uid"
     assert username == "valid-user"
     assert skip_user_id_check is False
+    assert token == "valid-token"  # noqa: S105
 
 
 @pytest.mark.usefixtures("_setup")
@@ -121,12 +122,13 @@ async def test_cluster_id_is_used_for_kube_admin(mock_authz_api):
             return_value="some-cluster-id",
         ),
     ):
-        user_uid, username, skip_user_id_check = await auth_dependency(request)
+        user_uid, username, skip_user_id_check, token = await auth_dependency(request)
 
     # check if the correct user info has been returned
     assert user_uid == "some-cluster-id"
     assert username == "kube:admin"
     assert skip_user_id_check is False
+    assert token == "valid-token"  # noqa: S105
 
 
 @pytest.mark.usefixtures("_setup")
