e33feb8b5ec56893be82426e3200ef9f4cb15ef6
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-05 11:15:04 +0100

    Quota limiters implementation

---
+++ ols/src/quota/quota_limiter.py
@@ -0,0 +1,46 @@
+"""Abstract class that is parent for all quota limiter implementations."""
+
+from abc import ABC, abstractmethod
+
+import psycopg2
+
+from ols.app.models.config import PostgresConfig
+
+
+class QuotaLimiter(ABC):
+    """Abstract class that is parent for all quota limiter implementations."""
+
+    @abstractmethod
+    def available_quota(self) -> int:
+        """Retrieve available quota for given user."""
+
+    @abstractmethod
+    def revoke_quota(self) -> None:
+        """Revoke quota for given user."""
+
+    @abstractmethod
+    def increase_quota(self) -> None:
+        """Increase quota for given user."""
+
+    @abstractmethod
+    def ensure_available_quota(self) -> None:
+        """Ensure that there's avaiable quota left."""
+
+    @abstractmethod
+    def consume_tokens(self, input_tokens: int, output_tokens: int) -> None:
+        """Consume tokens by given user."""
+
+    # pylint: disable=W0201
+    def connect(self, config: PostgresConfig) -> None:
+        """Initialize connection to database."""
+        self.connection = psycopg2.connect(
+            host=config.host,
+            port=config.port,
+            user=config.user,
+            password=config.password,
+            dbname=config.dbname,
+            sslmode=config.ssl_mode,
+            # sslrootcert=config.ca_cert_path,
+            gssencmode=config.gss_encmode,
+        )
+        self.connection.autocommit = True
