dd7b7c1f8653f0dc9591b3bf741b32e42113c61f
Haoyu Sun <hasun@redhat.com>
2025-03-12 12:48:28 +0100

    IndexLoader loads multiple indexes and provide a QueryFusionRetriever to query all indexes.
    Signed-off-by: Haoyu Sun <hasun@redhat.com>

--- ols/app/endpoints/ols.py
+++ ols/app/endpoints/ols.py
@@ -484,10 +484,13 @@ def generate_response(
         history = CacheEntry.cache_entries_to_history(previous_input)
         if streaming:
             return docs_summarizer.generate_response(
-                llm_request.query, config.rag_index, history
+                llm_request.query, config.rag_index_loader.get_retriever(), history
             )
         response = docs_summarizer.create_response(
-            llm_request.query, config.rag_index, history, user_token
+            llm_request.query,
+            config.rag_index_loader.get_retriever(),
+            history,
+            user_token,
         )
         logger.debug("%s Generated response: %s", conversation_id, response)
         return response
--- tests/unit/rag_index/test_index_loader.py
+++ tests/unit/rag_index/test_index_loader.py
@@ -1,6 +1,5 @@
 """Unit test for the index loader module."""
 
-from pathlib import Path
 from unittest.mock import patch
 
 from ols import config
@@ -12,11 +11,11 @@ from tests.mock_classes.mock_llama_index import MockLlamaIndex
 def test_index_loader_empty_config(caplog):
     """Test index loader with empty/None config."""
     index_loader_obj = IndexLoader(None)
-    index = index_loader_obj.vector_index
+    indexes = index_loader_obj.vector_indexes
 
     assert "required parameters are not set" in caplog.text
     assert not hasattr(index_loader_obj, "_embed_model")
-    assert index is None
+    assert indexes is None
 
 
 def test_index_loader_no_id():
@@ -30,13 +29,13 @@ def test_index_loader_no_id():
 
     with patch("llama_index.core.StorageContext.from_defaults"):
         index_loader_obj = IndexLoader(config.ols_config.reference_content)
-        index = index_loader_obj.vector_index
+        indexes = index_loader_obj.vector_indexes
 
         assert (
             index_loader_obj._embed_model
             == "local:sentence-transformers/all-mpnet-base-v2"
         )
-        assert index is None
+        assert indexes is None
 
 
 def test_index_loader():
@@ -50,8 +49,6 @@ def test_index_loader():
         ) as from_persist_dir,
         patch("llama_index.core.load_index_from_storage", new=MockLlamaIndex),
     ):
-        from_persist_dir.return_value = None
-
         config.ols_config.reference_content.indexes = [
             ReferenceContentIndex(
                 {
@@ -63,6 +60,7 @@ def test_index_loader():
         from_persist_dir.return_value = None
 
         index_loader_obj = IndexLoader(config.ols_config.reference_content)
-        index = index_loader_obj.vector_index
+        indexes = index_loader_obj.vector_indexes
 
-        assert isinstance(index, MockLlamaIndex)
+        assert len(indexes) == 1
+        assert isinstance(indexes[0], MockLlamaIndex)
