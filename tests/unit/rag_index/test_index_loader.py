1087159c4c13c56981914efd5656f2252c0f5426
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-25 20:31:27 +0100

    Fixed index loader test

--- tests/unit/rag_index/test_index_loader.py
+++ tests/unit/rag_index/test_index_loader.py
@@ -4,8 +4,7 @@ from pathlib import Path
 from unittest.mock import patch
 
 from ols import config
-from ols.app.models.config import PostgresConfig, ReferenceContent
-from ols.constants import VectorStoreType
+from ols.app.models.config import ReferenceContent
 from ols.src.rag_index.index_loader import IndexLoader
 from tests.mock_classes.mock_llama_index import MockLlamaIndex
 
@@ -50,52 +49,6 @@ def test_index_loader():
         patch("llama_index.core.load_index_from_storage", new=MockLlamaIndex),
     ):
         from_persist_dir.return_value = None
-
-        index_loader_obj = IndexLoader(config.ols_config.reference_content)
-        index = index_loader_obj.vector_index
-
-        assert isinstance(index, MockLlamaIndex)
-
-
-def test_index_loader_from_faiss():
-    """Test index loader when 'faiss' is selected for the vector store type."""
-    config.ols_config.reference_content = ReferenceContent(None)
-    config.ols_config.reference_content.vector_store_type = VectorStoreType.FAISS
-    config.ols_config.reference_content.product_docs_index_path = Path("./some_dir")
-    config.ols_config.reference_content.product_docs_index_id = "./some_id"
-
-    with (
-        patch("llama_index.core.StorageContext.from_defaults"),
-        patch(
-            "llama_index.vector_stores.faiss.FaissVectorStore.from_persist_dir"
-        ) as from_persist_dir,
-        patch("llama_index.core.load_index_from_storage", new=MockLlamaIndex),
-    ):
-        from_persist_dir.return_value = None
-
-        index_loader_obj = IndexLoader(config.ols_config.reference_content)
-        index = index_loader_obj.vector_index
-
-        assert isinstance(index, MockLlamaIndex)
-
-
-def test_index_loader_from_postgres():
-    """Test index loader when 'postgres' is selected for the vector store type."""
-    config.ols_config.reference_content = ReferenceContent(None)
-    config.ols_config.reference_content.vector_store_type = VectorStoreType.POSTGRES
-    config.ols_config.reference_content.product_docs_index_id = "some_id"
-    config.ols_config.reference_content.postgres = PostgresConfig()
-
-    with (
-        patch("llama_index.core.StorageContext.from_defaults"),
-        patch(
-            "llama_index.vector_stores.postgres.PGVectorStore.from_params"
-        ) as from_params,
-        patch(
-            "llama_index.core.VectorStoreIndex.from_vector_store", new=MockLlamaIndex
-        ),
-    ):
-        from_params.return_value = None
 
         index_loader_obj = IndexLoader(config.ols_config.reference_content)
         index = index_loader_obj.vector_index
