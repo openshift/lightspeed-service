b7ba3b937009f066c259c4a27ed6bb8d6e89c5d6
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-03-26 14:44:00 +0100

    Removed @patch decorator from utils unit tests

--- tests/unit/utils/test_environments.py
+++ tests/unit/utils/test_environments.py
@@ -7,53 +7,53 @@ from ols.app.models.config import OLSConfig, ReferenceContent
 from ols.utils.environments import configure_gradio_ui_envs, configure_hugging_face_envs
 
 
-@patch.dict(os.environ, {"GRADIO_ANALYTICS_ENABLED": "", "MPLCONFIGDIR": ""})
 def test_configure_gradio_ui_envs():
     """Test the function configure_gradio_ui_envs."""
-    # setup before tested function is called
-    assert os.environ.get("GRADIO_ANALYTICS_ENABLED", None) == ""
-    assert os.environ.get("MPLCONFIGDIR", None) == ""
+    with patch.dict(os.environ, {"GRADIO_ANALYTICS_ENABLED": "", "MPLCONFIGDIR": ""}):
+        # setup before tested function is called
+        assert os.environ.get("GRADIO_ANALYTICS_ENABLED", None) == ""
+        assert os.environ.get("MPLCONFIGDIR", None) == ""
 
-    # call the tested function
-    configure_gradio_ui_envs()
+        # call the tested function
+        configure_gradio_ui_envs()
 
-    # expected environment variables
-    assert os.environ.get("GRADIO_ANALYTICS_ENABLED", None) == "false"
-    assert os.environ.get("MPLCONFIGDIR", None) != ""
+        # expected environment variables
+        assert os.environ.get("GRADIO_ANALYTICS_ENABLED", None) == "false"
+        assert os.environ.get("MPLCONFIGDIR", None) != ""
 
 
-@patch.dict(os.environ, {"TRANSFORMERS_CACHE": "", "TRANSFORMERS_OFFLINE": ""})
 def test_configure_hugging_face_env_no_reference_content_set():
     """Test the function configure_hugging_face_envs."""
-    # setup before tested function is called
-    assert os.environ.get("TRANSFORMERS_CACHE", None) == ""
-    assert os.environ.get("TRANSFORMERS_OFFLINE", None) == ""
+    with patch.dict(os.environ, {"TRANSFORMERS_CACHE": "", "TRANSFORMERS_OFFLINE": ""}):
+        # setup before tested function is called
+        assert os.environ.get("TRANSFORMERS_CACHE", None) == ""
+        assert os.environ.get("TRANSFORMERS_OFFLINE", None) == ""
 
-    ols_config = OLSConfig()
-    ols_config.reference_content = None
+        ols_config = OLSConfig()
+        ols_config.reference_content = None
 
-    # call the tested function
-    configure_hugging_face_envs(ols_config)
+        # call the tested function
+        configure_hugging_face_envs(ols_config)
 
-    # expected environment variables
-    assert os.environ.get("TRANSFORMERS_CACHE", None) == ""
-    assert os.environ.get("TRANSFORMERS_OFFLINE", None) == ""
+        # expected environment variables
+        assert os.environ.get("TRANSFORMERS_CACHE", None) == ""
+        assert os.environ.get("TRANSFORMERS_OFFLINE", None) == ""
 
 
-@patch.dict(os.environ, {"TRANSFORMERS_CACHE": "", "TRANSFORMERS_OFFLINE": ""})
 def test_configure_hugging_face_env_reference_content_set():
     """Test the function configure_hugging_face_envs."""
-    # setup before tested function is called
-    assert os.environ.get("TRANSFORMERS_CACHE", None) == ""
-    assert os.environ.get("TRANSFORMERS_OFFLINE", None) == ""
+    with patch.dict(os.environ, {"TRANSFORMERS_CACHE": "", "TRANSFORMERS_OFFLINE": ""}):
+        # setup before tested function is called
+        assert os.environ.get("TRANSFORMERS_CACHE", None) == ""
+        assert os.environ.get("TRANSFORMERS_OFFLINE", None) == ""
 
-    ols_config = OLSConfig()
-    ols_config.reference_content = ReferenceContent()
-    ols_config.reference_content.embeddings_model_path = "foo"
+        ols_config = OLSConfig()
+        ols_config.reference_content = ReferenceContent()
+        ols_config.reference_content.embeddings_model_path = "foo"
 
-    # call the tested function
-    configure_hugging_face_envs(ols_config)
+        # call the tested function
+        configure_hugging_face_envs(ols_config)
 
-    # expected environment variables
-    assert os.environ.get("TRANSFORMERS_CACHE", None) == "foo"
-    assert os.environ.get("TRANSFORMERS_OFFLINE", None) == "1"
+        # expected environment variables
+        assert os.environ.get("TRANSFORMERS_CACHE", None) == "foo"
+        assert os.environ.get("TRANSFORMERS_OFFLINE", None) == "1"
