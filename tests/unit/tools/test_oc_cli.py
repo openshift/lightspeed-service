e2fd2de5cf7d2c2f981bafa43de533d15e84c376
Ondrej Metelka <ometelka@redhat.com>
2025-04-17 09:08:24 +0200

    Remove check if token works for oc tools

--- tests/unit/tools/test_oc_cli.py
+++ tests/unit/tools/test_oc_cli.py
@@ -1,11 +1,8 @@
 """Test cases for the oc_cli module."""
 
-import subprocess
-from unittest.mock import patch
-
 import pytest
 
-from ols.src.tools.oc_cli import sanitize_oc_args, token_works_for_oc
+from ols.src.tools.oc_cli import sanitize_oc_args
 
 
 @pytest.mark.parametrize(
@@ -25,21 +22,3 @@ from ols.src.tools.oc_cli import sanitize_oc_args, token_works_for_oc
 def test_sanitize_oc_args(input_args, expected_output):
     """Test that oc args are sanitized."""
     assert sanitize_oc_args(input_args) == expected_output
-
-
-def test_token_works_for_oc_failure():
-    """Test token_works_for_oc failure scenario."""
-    with patch(
-        "ols.src.tools.oc_cli.run_oc",
-        side_effect=subprocess.CalledProcessError(1, "oc"),
-    ):
-        assert not token_works_for_oc("some-token")
-
-
-def test_token_works_for_oc_success():
-    """Test token_works_for_oc success scenario."""
-    with patch(
-        "ols.src.tools.oc_cli.run_oc",
-        returns_value=subprocess.CompletedProcess("oc", 0),
-    ):
-        assert token_works_for_oc("some-token")
