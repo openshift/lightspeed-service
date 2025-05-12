70c9a7317a6d3ddbf4d4fb6c3606ae6ccef03184
Pavel Tisnovsky <ptisnovs@redhat.com>
2025-05-12 09:09:30 +0200

    Use proper response object in benchmark

--- tests/benchmarks/test_errors_parsing.py
+++ tests/benchmarks/test_errors_parsing.py
@@ -1,7 +1,7 @@
 """Benchmarks for errors parsing feature."""
 
 from genai.exceptions import ApiResponseException
-from httpx import Response
+from httpx import Request, Response
 from openai import BadRequestError
 
 from ols.utils import errors_parsing
@@ -10,7 +10,10 @@ from ols.utils import errors_parsing
 def test_parse_generic_llm_error_on_bad_request_error_without_info(benchmark):
     """Benchmark the parse_generic_llm_error function when BadRequestError is passed."""
     status_code = 400
-    response = Response(status_code=status_code, request={})
+    response = Response(
+        status_code=status_code,
+        request=Request(method="GET", url="http://foo.com"),
+    )
     error = BadRequestError("Exception", response=response, body=None)
 
     # benchmark the function to parse LLM errors
@@ -21,7 +24,10 @@ def test_parse_generic_llm_error_on_bad_request_error_with_info(benchmark):
     """Benchmark the parse_generic_llm_error function when BadRequestError is passed."""
     status_code = 400
     message = "Exception message"
-    response = Response(status_code=status_code, request={})
+    response = Response(
+        status_code=status_code,
+        request=Request(method="GET", url="http://foo.com"),
+    )
     error = BadRequestError("Exception", response=response, body={"message": message})
 
     # benchmark the function to parse LLM errors
