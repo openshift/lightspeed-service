030f5658ffb74b50bf4b37a469df6f849f9115e6
Joao Fula <jfula@redhat.com>
2025-05-20 14:58:32 +0200

    Modifying script calling and installation for disconnected environment
    Modifying script calling and installation for disconnected environment
    
    Modifying script calling and installation for disconnected environment
    
    changing installation of operator

--- tests/scripts/test-e2e-cluster-periodics.sh
+++ tests/scripts/test-e2e-cluster-periodics.sh
@@ -37,36 +37,41 @@ function run_suites() {
   # runsuite arguments:
   # suiteid test_tags provider provider_keypath model ols_image
   # empty test_tags means run all tests
-  run_suite "azure_openai" "not certificates and not (tool_calling and not smoketest and not rag)" "azure_openai" "$AZUREOPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "n"
-  (( rc = rc || $? ))
+  if [$DISCONNECTED]; then
+    # smoke tests for RHOAI VLLM-compatible provider
+    run_suite "rhoai_vllm" "smoketest" "rhoai_vllm" "$OPENAI_PROVIDER_KEY_PATH" "gpt-3.5-turbo" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
+  else
+    run_suite "azure_openai" "not certificates and not (tool_calling and not smoketest and not rag)" "azure_openai" "$AZUREOPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
 
-  run_suite "openai" "not azure_entra_id and not certificates and not (tool_calling and not smoketest and not rag)" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "n"
-  (( rc = rc || $? ))
+    run_suite "openai" "not azure_entra_id and not certificates and not (tool_calling and not smoketest and not rag)" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
 
-  run_suite "watsonx" "not azure_entra_id and not certificates and not (tool_calling and not smoketest and not rag)" "watsonx" "$WATSONX_PROVIDER_KEY_PATH" "ibm/granite-3-2-8b-instruct" "$OLS_IMAGE" "n"
-  (( rc = rc || $? ))
+    run_suite "watsonx" "not azure_entra_id and not certificates and not (tool_calling and not smoketest and not rag)" "watsonx" "$WATSONX_PROVIDER_KEY_PATH" "ibm/granite-3-2-8b-instruct" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
 
-  # smoke tests for RHOAI VLLM-compatible provider
-  run_suite "rhoai_vllm" "smoketest" "rhoai_vllm" "$OPENAI_PROVIDER_KEY_PATH" "gpt-3.5-turbo" "$OLS_IMAGE" "n"
-  (( rc = rc || $? ))
+    # smoke tests for RHOAI VLLM-compatible provider
+    run_suite "rhoai_vllm" "smoketest" "rhoai_vllm" "$OPENAI_PROVIDER_KEY_PATH" "gpt-3.5-turbo" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
 
-  # smoke tests for RHELAI VLLM-compatible provider
-  run_suite "rhelai_vllm" "smoketest" "rhelai_vllm" "$OPENAI_PROVIDER_KEY_PATH" "gpt-3.5-turbo" "$OLS_IMAGE" "n"
-  (( rc = rc || $? ))
-  
-  run_suite "certificates" "certificates" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "n"
-  (( rc = rc || $? ))
+    # smoke tests for RHELAI VLLM-compatible provider
+    run_suite "rhelai_vllm" "smoketest" "rhelai_vllm" "$OPENAI_PROVIDER_KEY_PATH" "gpt-3.5-turbo" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
+    
+    run_suite "certificates" "certificates" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "n"
+    (( rc = rc || $? ))
 
 
-  # TODO: Reduce execution time. Sequential execution will take more time. Parallel execution will have cluster claim issue.
-  # Run tool calling - Enable tool_calling
-  run_suite "azure_openai_tool_calling" "tool_calling" "azure_openai" "$AZUREOPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "y"
-  (( rc = rc || $? ))
-  run_suite "openai_tool_calling" "tool_calling" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "y"
-  (( rc = rc || $? ))
-  run_suite "watsonx_tool_calling" "tool_calling" "watsonx" "$WATSONX_PROVIDER_KEY_PATH" "ibm/granite-3-2-8b-instruct" "$OLS_IMAGE" "y"
-  (( rc = rc || $? ))
-
+    # TODO: Reduce execution time. Sequential execution will take more time. Parallel execution will have cluster claim issue.
+    # Run tool calling - Enable tool_calling
+    run_suite "azure_openai_tool_calling" "tool_calling" "azure_openai" "$AZUREOPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "y"
+    (( rc = rc || $? ))
+    run_suite "openai_tool_calling" "tool_calling" "openai" "$OPENAI_PROVIDER_KEY_PATH" "gpt-4o-mini" "$OLS_IMAGE" "y"
+    (( rc = rc || $? ))
+    run_suite "watsonx_tool_calling" "tool_calling" "watsonx" "$WATSONX_PROVIDER_KEY_PATH" "ibm/granite-3-2-8b-instruct" "$OLS_IMAGE" "y"
+    (( rc = rc || $? ))
+  fi
   set -e
 
   return $rc
